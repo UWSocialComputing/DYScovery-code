bd075a18cf6752255b300c5e1753620642cc8136
Shreya Jayaraman <shreyaj@Shreyas-MacBook-Pro-3.local>
2023-05-08 12:21:39 -0700

    begin outlining DetailPage

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -1,11 +1,33 @@
 import * as React from "react";
 import Title from "../../components/Title/Title";
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import InfoIcon from '@mui/icons-material/Info';
+import TuneIcon from '@mui/icons-material/Tune';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import ReviewsIcon from '@mui/icons-material/Reviews';
+import ChatIcon from '@mui/icons-material/Chat';
 
 function DetailPage() {
+  var value = "hello";
+
+  const handleChange = (event) => {
+    // setname(event.target.value);
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-8">
       <Title />
-      <div>Details Page Has Nothing Yet</div>
+      
+      <Tabs value={value} onChange={handleChange} aria-label="icon label tabs example">
+        <Tab icon={<InfoIcon />} label="ABOUT" />
+        <Tab icon={<TuneIcon />} label="PREFERENCES" />
+        <Tab icon={<LocationOnIcon />} label="NEIGHBORHOOD" />
+        <Tab icon={<ReviewsIcon />} label="REVIEWS" />
+        <Tab icon={<ChatIcon />} label="CHAT" />
+      </Tabs>
+
+      <div>Details Page in progress!!!</div>
     </div>
   );
 }
