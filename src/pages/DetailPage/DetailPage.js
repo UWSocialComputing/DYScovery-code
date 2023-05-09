c1f9260a8dff943308256a8b7d936e0a25174ee8
Shreya Jayaraman <shreya.jayaraman4@gmail.com>
2023-05-09 12:29:13 -0700

    add card components to details page

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -7,6 +7,12 @@ import TuneIcon from '@mui/icons-material/Tune';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import ReviewsIcon from '@mui/icons-material/Reviews';
 import ChatIcon from '@mui/icons-material/Chat';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function DetailPage() {
   var value = "hello";
@@ -16,9 +22,10 @@ function DetailPage() {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-8">
+    <div className="min-h-screen flex-col items-center justify-between p-8">
       <Title />
-      
+
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
       <Tabs value={value} onChange={handleChange} aria-label="icon label tabs example">
         <Tab icon={<InfoIcon />} label="ABOUT" />
         <Tab icon={<TuneIcon />} label="PREFERENCES" />
@@ -26,8 +33,72 @@ function DetailPage() {
         <Tab icon={<ReviewsIcon />} label="REVIEWS" />
         <Tab icon={<ChatIcon />} label="CHAT" />
       </Tabs>
+      </div>
 
-      <div>Details Page in progress!!!</div>
+      <Card variant="outlined">
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            About
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Insert text here.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+
+    <Card variant="outlined">
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Preferences
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Insert text here.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+
+    <Card variant="outlined">
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Neighborhood
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Insert text here.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+
+    <Card variant="outlined">
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Reviews
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Insert text here.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+
+    <Card variant="outlined">
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Social Media
+          </Typography>
+            <InstagramIcon />
+            <TwitterIcon />
+        </CardContent>
+      </CardActionArea>
+    </Card>
+
     </div>
   );
 }
