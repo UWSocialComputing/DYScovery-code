29446a8242df50b9a4f7a10e678859a854124209
Shreya Jayaraman <shreya.jayaraman4@gmail.com>
2023-05-11 12:41:22 -0700

    change details page

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -1,104 +1,200 @@
 import * as React from "react";
 import Title from "../../components/Title/Title";
-import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
-import InfoIcon from '@mui/icons-material/Info';
-import TuneIcon from '@mui/icons-material/Tune';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import ReviewsIcon from '@mui/icons-material/Reviews';
-import ChatIcon from '@mui/icons-material/Chat';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
 
 function DetailPage() {
-  var value = "hello";
+  const navigation = [
+    { name: 'About', href: '#', current: true },
+    { name: 'Preferences', href: '#', current: false },
+    { name: 'Neighborhood', href: '#', current: false },
+    { name: 'Reviews', href: '#', current: false },
+    { name: 'Social Media', href: '#', current: false}
+  ]
 
-  const handleChange = (event) => {
-    console.log("hello");
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
   }
+
+  const details = [
+    {
+      name: 'About',
+      text: 'Insert about text here.',
+    },
+    {
+      name: 'Preferences',
+      text: 'Insert about text here.'
+    },
+    {
+      name: 'Neighborhood',
+      text: 'Insert about text here.'
+    },
+    {
+      name: 'Reviews',
+      text: 'Insert about text here.'
+    },
+    {
+      name: 'Social Media',
+      text: 'Insert about text here.'
+    },
+  ]
 
   return (
     <div className="min-h-screen flex-col items-center justify-between p-8">
       <Title />
 
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-      <Tabs value={value} onChange={handleChange} aria-label="icon label tabs example">
-        <Tab icon={<InfoIcon />} label="ABOUT" />
-        <Tab icon={<TuneIcon />} label="PREFERENCES" />
-        <Tab icon={<LocationOnIcon />} label="NEIGHBORHOOD" />
-        <Tab icon={<ReviewsIcon />} label="REVIEWS" />
-        <Tab icon={<ChatIcon />} label="CHAT" />
-      </Tabs>
-      </div>
+      <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="block h-8 w-auto lg:hidden"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                  />
+                  <img
+                    className="hidden h-8 w-auto lg:block"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
 
-      <Card variant="outlined">
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            About
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Insert text here.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Your Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
 
-    <Card variant="outlined">
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Preferences
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Insert text here.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
 
-    <Card variant="outlined">
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Neighborhood
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Insert text here.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-
-    <Card variant="outlined">
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Reviews
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Insert text here.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-
-    <Card variant="outlined">
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Social Media
-          </Typography>
-            <InstagramIcon />
-            <TwitterIcon />
-        </CardContent>
-      </CardActionArea>
-    </Card>
-
+    <ul role="list" className="divide-y divide-gray-100">
+      {details.map((detail) => (
+        <li key={detail.name} className="flex justify-between gap-x-6 py-5">
+          <div className="flex gap-x-4">
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-900">{detail.name}</p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{detail.text}</p>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+    
     </div>
   );
 }
