4b13be7fcff36906ee197719b3e8d121070c3629
Diana Dai <dianadai.dy@gmail.com>
2023-05-16 12:32:58 -0700

    remove the learn more button for verified ticket

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -276,12 +276,6 @@ function DetailPage() {
             </svg>
             User's event ticket has been verified through TicketMaster.
           </p>
-          <a
-            href="#"
-            className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
-          >
-            Learn more! <span aria-hidden="true">&rarr;</span>
-          </a>
         </div>
       </div>
 
