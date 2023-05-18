7bc754a2cd3db6932342168158f1701998971c6f
Diana Dai <dianadai.dy@gmail.com>
2023-05-18 09:24:21 -0700

    add different user ratings toggle

--- src/components/DetailPageInformation/ReviewDetail.js
+++ src/components/DetailPageInformation/ReviewDetail.js
@@ -1,48 +1,50 @@
 import React from "react";
 import { Rating } from "flowbite-react";
 
-const ReviewDetail = ({ name, date, rating, review, helpfulCount }) => {
+function renderRatingStars(rating) {
   const starComponents = Array.from({ length: 5 }, (_, index) => (
     <Rating.Star key={index} filled={index < rating} />
   ));
 
+  return starComponents;
+}
+
+const ReviewDetail = ({ review }) => {
   return (
     <article>
       <div className="flex items-center mb-4 space-x-4">
         <img
           className="w-10 h-10 rounded-full"
-          src={
-            name === "Jessie L."
-              ? "https://cdn.pixabay.com/photo/2020/11/25/10/30/phone-5775234_960_720.png"
-              : "https://cdn.pixabay.com/photo/2020/12/20/03/58/girl-5846034_960_720.jpg"
-          }
+          src={review.profileImage}
           alt=""
         />
         <div className="space-y-1 font-medium dark:text-white">
           <p>
-            {name}{" "}
+            {review.name}{" "}
             <time
               dateTime="2014-08-16 19:00"
               className="block text-sm text-gray-500 dark:text-gray-400"
             >
-              Joined {date}
+              Joined {review.joinedDate}
             </time>
           </p>
         </div>
       </div>
       <div className="flex items-center mb-1">
-        <Rating>{starComponents}</Rating>
+        <Rating>{renderRatingStars(review.rating)}</Rating>
         <h3 className="ml-2 text-sm font-semibold text-gray-900 dark:text-white">
-          Favorite roommates!
+          {review.reviewTitle}
         </h3>
       </div>
       <footer className="mb-5 text-sm text-gray-500 dark:text-gray-400">
-        <p>Reviewed on {date}</p>
+        <p>Reviewed on {review.date}</p>
       </footer>
-      <p className="mb-2 text-gray-500 dark:text-gray-400">{review}</p>
+      <p className="mb-2 text-gray-500 dark:text-gray-400">
+        {review.reviewContent}
+      </p>
       <aside>
         <p className="mt-1 text-xs text-gray-500 dark:text-gray-400">
-          {helpfulCount} people found this helpful
+          {review.helpfulCount} people found this helpful
         </p>
         <div className="flex items-center mt-3 space-x-3 divide-x divide-gray-200 dark:divide-gray-600">
           <a
--- src/components/DetailPageInformation/ReviewInfo.js
+++ src/components/DetailPageInformation/ReviewInfo.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Card, Rating } from "flowbite-react";
+import React, { useState } from "react";
+import { Card, Rating, Avatar } from "flowbite-react";
 import ReviewDetail from "./ReviewDetail";
 
 function renderRatingBars(ratings, criteria) {
@@ -33,53 +33,149 @@ function renderRatingBars(ratings, criteria) {
   );
 }
 
+function renderRatingStars(rating) {
+  const starComponents = Array.from({ length: 5 }, (_, index) => (
+    <Rating.Star key={index} filled={index + 1 < rating} />
+  ));
+
+  return starComponents;
+}
+
 function ReviewInfo() {
-  const ratings = [4.8, 5.0, 4.5, 5.0];
-  const criteria = [
-    "Cleanliness",
-    "Communication",
-    "Trustworthiness",
-    "Respectfulness",
+  const userInformation = [
+    {
+      profileImage:
+        "https://flowbite.com/docs/images/people/profile-picture-3.jpg",
+      name: "Ella C.",
+      joinedDate: "August 2022",
+      ratings: [4.8, 5.0, 4.5, 5.0],
+      criteria: [
+        "Cleanliness",
+        "Communication",
+        "Trustworthiness",
+        "Respectfulness",
+      ],
+      reviews: [
+        {
+          name: "Julia L.",
+          profileImage:
+            "https://cdn.pixabay.com/photo/2020/12/20/03/58/girl-5846034_960_720.jpg",
+          joinedDate: "August 2022",
+          helpfulCount: 2,
+          rating: 5,
+          date: "May 14, 2023",
+          reviewTitle: "Favorite roommates!",
+          reviewContent:
+            "Ella and Rory were fab roomies! They were patient and clean. 10/10 would room together again.",
+        },
+        {
+          name: "Arya M.",
+          profileImage:
+            "https://cdn.pixabay.com/photo/2020/11/25/10/30/phone-5775234_960_720.png",
+          joinedDate: "Feb 2023",
+          helpfulCount: 1,
+          rating: 4,
+          date: "March 3, 2023",
+          reviewTitle: "Cool people",
+          reviewContent:
+            "Had a great experience living with Ella. She was very friendly and respectful. The only downside was that the Wi-Fi was a bit slow at times.",
+        },
+      ],
+    },
+    {
+      profileImage:
+        "https://st.depositphotos.com/1144472/1532/i/600/depositphotos_15320783-stock-photo-portrait-of-young-woman-at.jpg",
+      name: "Rory R.",
+      joinedDate: "Feb 2023",
+      ratings: [4.0, 4.2, 3.8, 4.5],
+      criteria: [
+        "Cleanliness",
+        "Communication",
+        "Trustworthiness",
+        "Respectfulness",
+      ],
+      reviews: [
+        {
+          name: "Arya M.",
+          profileImage:
+            "https://cdn.pixabay.com/photo/2020/11/25/10/30/phone-5775234_960_720.png",
+          joinedDate: "Feb 2023",
+          helpfulCount: 1,
+          rating: 4,
+          date: "March 3, 2023",
+          reviewTitle: "Cool people",
+          reviewContent:
+            "Had a great experience living with Ella. She was very friendly and respectful. The only downside was that the Wi-Fi was a bit slow at times.",
+        },
+      ],
+    },
   ];
 
+  const [selectedAvatarIndex, setSelectedAvatarIndex] = useState(null);
+
+  const handleAvatarClick = (index) => {
+    if (index === selectedAvatarIndex) {
+      setSelectedAvatarIndex(null); // Deselect if the same avatar is clicked again
+    } else {
+      setSelectedAvatarIndex(index);
+    }
+  };
+
   return (
-    <Card id={"reviews"}>
+    <Card id="reviews">
       <h2 className="text-xl font-bold dark:text-white">User Reviews</h2>
+      <p>Click on the avatar to see more. </p>
+      <div id="detailedreviews" className="grid grid-cols-4">
+        {userInformation.map((user, index) => (
+          <div
+            key={index}
+            className={`flex items-center gap-2 p-3 rounded-lg cursor-pointer ${
+              selectedAvatarIndex === index ? "bg-gray-200" : ""
+            }`}
+            onClick={() => handleAvatarClick(index)}
+          >
+            <Avatar img={user.profileImage} rounded={true} />
+            <p className="">{user.name}</p>
+          </div>
+        ))}
+      </div>
       <div className="flex">
         <div className="flex-1 font-normal text-sm text-gray-700 dark:text-gray-400 pr-8">
-          <dt className="text font-medium text-gray-500 dark:text-gray-400 mb-1">
-            Overall
-          </dt>
-          <Rating>
-            <Rating.Star />
-            <Rating.Star />
-            <Rating.Star />
-            <Rating.Star />
-            <Rating.Star filled={false} />
-            <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
-              4.80 out of 5
-            </p>
-          </Rating>
-          {renderRatingBars(ratings, criteria)}
+          {selectedAvatarIndex !== null && (
+            <>
+              <dt className="text font-medium text-gray-500 dark:text-gray-400 mb-1">
+                Overall
+              </dt>
+              <Rating>
+                {renderRatingStars(
+                  userInformation[selectedAvatarIndex].ratings.reduce(
+                    (total, rating) => total + rating,
+                    0
+                  ) / userInformation[selectedAvatarIndex].ratings.length
+                )}
+                <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
+                  {(
+                    userInformation[selectedAvatarIndex].ratings.reduce(
+                      (total, rating) => total + rating,
+                      0
+                    ) / userInformation[selectedAvatarIndex].ratings.length
+                  ).toFixed(2)}{" "}
+                  out of 5
+                </p>
+              </Rating>
+
+              {renderRatingBars(
+                userInformation[selectedAvatarIndex].ratings,
+                userInformation[selectedAvatarIndex].criteria
+              )}
+            </>
+          )}
         </div>
-        <div className="flex-1">
-          <div id="detailedreviews">
-            <ReviewDetail
-              name="Jessie L."
-              date="August 2022"
-              rating={5}
-              review="Ella and Rory were fab roomies! They were patient and clean. 10/10 would room together again."
-              helpfulCount={2}
-            />
-            <br />
-            <ReviewDetail
-              name="Arya M."
-              date="Feb 2023"
-              rating={4}
-              review="Had a great experience living with Ella. She was very friendly and respectful. The only downside was that the Wi-Fi was a bit slow at times."
-              helpfulCount={1}
-            />
-          </div>
+        <div className="flex-1 space-y-6">
+          {selectedAvatarIndex !== null &&
+            userInformation[selectedAvatarIndex].reviews.map(
+              (review, index) => <ReviewDetail review={review} key={index} />
+            )}
         </div>
       </div>
     </Card>
